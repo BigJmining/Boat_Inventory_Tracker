1fadc76d249491749f4b4ef99c0b90d15ba7f36e
Jason <jlohrey09@gmail.com>
2022-12-03 19:34:25 -0800

    folders made and files renamed

--- VesselInventory.py
+++ VesselInventory.py
@@ -3,7 +3,7 @@
 ## written by Jason Lohrey
 
 
-import json, io
+import json, io, os
 from PIL import Image
 
 
@@ -12,15 +12,17 @@ try:
 except NameError:
     to_unicode = str
 
+myCWD = os.getcwd()
 
 def get_storage_image():
-    img_boat1 = Image.open("vessel_layout_test1.jpeg") # 788 x 1976
+    img_boat1 = Image.open(os.path.join(myCWD,"images","vessel_layout_test1.jpeg")) # 788 x 1976
     # img_X, img_Y = (788,1976)
     
     bow_storage = (300,0,500,210)
     stern_storage = (30,1785,700,1785)
     port_storage = (30,700,300,1000)
     starboard_storage = (400,700,700,1000)
+    other_storage = (0,0,0,0)
 
     if vessel_inventory['tools'][get_tool_info][1] == 'bow':
         storage_selection = img_boat1.crop(bow_storage)
@@ -30,6 +32,9 @@ def get_storage_image():
         storage_selection = img_boat1.crop(port_storage)
     if vessel_inventory['tools'][get_tool_info][1] == 'starboard':
         storage_selection = img_boat1.crop(starboard_storage)
+    else:
+        storage_selection = img_boat1.crop(other_storage)
+
     try:
         storage_selection.show()
     except UnboundLocalError:
@@ -37,13 +42,10 @@ def get_storage_image():
 
 
 
-filename = 'tools.json'
+toolFile = 'tools.json'
 
-## for debugging use
-filename1 = 'testing.json'
-filename2 = 'inventory.json'
 
-with open(filename, 'r') as f:
+with open(os.path.join(myCWD,'toolBox',toolFile), 'r') as f:
 	data = f.read()
 	vessel_inventory = json.loads(data)
 
@@ -71,6 +73,7 @@ def print_inventory():
         else:
             print(k,'-', v[0], '-', v[1], 'storage')
     print('\n')
+    
 
 def remove_tool():
     pass
@@ -93,7 +96,7 @@ def add_new_tool():
             toolNEW['tools'][add_tool] = tool_to_add
             print(toolNEW)
 
-            with io.open(f"tools.json", 'w', encoding='utf8') as outfile:
+            with io.open(os.path.join(myCWD,'toolBox',toolFile), 'w', encoding='utf8') as outfile:
                                 str_ = json.dumps(toolNEW,
                                                     indent=4, 
                                                     sort_keys=True,
@@ -109,13 +112,14 @@ def add_new_tool():
                 pass
             else:
                 break
+
     if addingTOOL != 'y':
         print('Goodbye')
     
         
 #### check on inventory
 def check_inventory():
-    with open(filename, 'r') as f:
+    with open(os.path.join(myCWD,'toolBox',toolFile), 'r') as f:
         global vessel_inventory
         data = f.read()
         vessel_inventory = json.loads(data)
@@ -153,21 +157,21 @@ def main():
     print_intro()
 
     selection = input('>>>: ')
-    
-    if selection == 'a':
-        add_new_tool()
-    
-    if selection == 'b':
-        check_inventory()
-    
-    if selection == 'c':
-        print_inventory()
-    
-    if selection == 'd':
-        remove_tool()
+    if selection not in ['q','Q']:
+        if selection == 'a':
+            add_new_tool()
+        
+        if selection == 'b':
+            check_inventory()
+        
+        if selection == 'c':
+            print_inventory()
+        
+        if selection == 'd':
+            remove_tool()
 
-    if selection == 'n':
-        print('Goodbye')
+        if selection == 'n' or 'q':
+            print('Goodbye')
     
     
 
